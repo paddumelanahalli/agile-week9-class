65cc9f251df54a2b26c49a661aaee4e0f71eeda4
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2026-03-24 19:58:32 -0700

    Update environment.py

--- environment.py
+++ environment.py
@@ -20,3 +20,44 @@ def after_all(context):
     """ Runs once at the very end """
     print("\n[GLOBAL CLEANUP] Powering down Lab Simulation...")
     context.lab_active = False
+
+
+"""
+
+output
+
+>behave --no-capture
+USING RUNNER: behave.runner:Runner
+
+[GLOBAL SETUP] Initializing Cisco Lab Simulation...
+Feature: Basic Arithmetic # features/calculator.feature:2
+  As a student
+  I want to use a calculator
+  So that I can sum numbers correctly
+[SCENARIO START] Testing: Add two numbers
+
+  Scenario: Add two numbers     # features/calculator.feature:7
+    Given I have a calculator   # features/steps/calc_steps.py:5 0.000s
+    When I enter 5 and 7        # features/steps/calc_steps.py:10 0.000s
+    Then the total should be 12 # features/steps/calc_steps.py:14 0.000s
+
+[SCENARIO START] Testing: Adding multiple pairs -- @1.1 Numbers
+
+    Then the total should be 30                            # features/steps/calc_steps.py:14 0.000s
+    Then the total should be 30                            # features/steps/calc_steps.py:14
+[SCENARIO START] Testing: Adding multiple pairs -- @1.2 Numbers
+
+    Then the total should be 0                             # features/steps/calc_steps.py:14 0.000s
+    Then the total should be 0                             # features/steps/calc_steps.py:14
+[SCENARIO START] Testing: Adding multiple pairs -- @1.3 Numbers
+
+    Then the total should be 100                           # features/steps/calc_steps.py:14 0.000s
+    Then the total should be 100                           # features/steps/calc_steps.py:14
+
+[GLOBAL CLEANUP] Powering down Lab Simulation...
+1 feature passed, 0 failed, 0 skipped
+4 scenarios passed, 0 failed, 0 skipped
+12 steps passed, 0 failed, 0 skipped
+Took 0min 0.001s
+
+"""
